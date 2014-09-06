c5a3f6633ee4a6e519162394988d7e4cdec48fb8
Diederick C. Niehorster <all@LILILAB>
2014-09-06 23:16:55 +0800

    replaceIntervalVelocity can now either interpolate or replace with constant

--- replaceIntervalVelocity.m
+++ replaceIntervalVelocity.m
@@ -1,16 +1,24 @@
-function [vel,velX,velY] = replaceIntervalVelocity(vel,velX,velY,on,off)
+function [vel,velX,velY] = replaceIntervalVelocity(vel,velX,velY,on,off,val)
 % Linearly interpolate velocity between on and off
 % on and off are sample numbers (data indices) of beginning and end of
-% interval to be replaced by linear interpolation between begin and end
+% interval to be replaced by linear interpolation between begin and end, or
+% by constant value if input val is set
 
 npoint = off-on+1;
 
-% components: replace with linearly interpolated velocity
-velX(on:off) = linspace(velX(on),velX(off),npoint);
-velY(on:off) = linspace(velY(on),velY(off),npoint);
-
-% calculate interpolate 2D velocity. In effect this is now interpolated
-% with a bicubic spline. Thats fine, good even as no edges are introduced
-% into the data, as we care most about the component velocities in the
-% situations I can think of.
-vel(on:off)  = hypot(velX(on:off),velY(on:off));
+if nargin>5
+    % components: replace with constant velocity
+    velX(on:off) = val;
+    velY(on:off) = val;
+    vel(on:off)  = hypot(val,val);
+else
+    % components: replace with linearly interpolated velocity
+    velX(on:off) = linspace(velX(on),velX(off),npoint);
+    velY(on:off) = linspace(velY(on),velY(off),npoint);
+    
+    % calculate interpolate 2D velocity. In effect this is now interpolated
+    % with a bicubic spline. Thats fine, good even as no edges are introduced
+    % into the data, as we care most about the component velocities in the
+    % situations I can think of.
+    vel(on:off)  = hypot(velX(on:off),velY(on:off));
+end
