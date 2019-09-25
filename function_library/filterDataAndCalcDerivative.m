e307d138148125131abcdbb6de2001f5ed597bff
Diederick C. Niehorster <dcnieho@gmail.com>
2019-09-26 01:43:58 +0200

    now have probably correct eye acceleration

--- function_library/filterDataAndCalcDerivative.m
+++ function_library/filterDataAndCalcDerivative.m
@@ -15,57 +15,6 @@ function data = filterDataAndCalcDerivative(data,ETparams)
 % correct value of the derivative.
 % See also sections 5.7 (on calculating numerical derivatives) and 14.8 (on
 % Savitzky-Golay filters) in Numerical recipes in C++
-%
-% Note that Numerical Recipes recommends using a polynomial order of at
-% least 4 when you're interested in derivatives of the data. Savitzky-Golay
-% filters however introduce false negative peaks at the edges of large
-% positive peaks. These might then be detected as false glissades in our
-% algorithm as they become positive peaks after the saccade when
-% calculating the 2D velocity magnitude. We therefore use a second order
-% polynomial only, which has LESS of this problem, but is still affected by
-% it. I am not sure about the extent to which this increases the number of
-% glissades found, i.e., how many of the detected glissades are just
-% artifacts of the filter. We might look at weighting the polynomial
-% regression filter coefficients, as supported by the savitzkyGolay()
-% function.
-% This brings with it another problem. Savitzki-Golay filters of polynomial
-% order N preserve up to the Nth order moment of their input data. 0th
-% moment: area under a peak, 1st order: position of the peak, 2nd order
-% moment: width of the peak. I can't find more, but I'd bet that the 3th
-% order is the width of the velocity peak. This means that although the
-% location of the velocity peak is preserved, its width is not. Indeed,
-% comparing a first order polynomial filter on position to the performance
-% of a second order filter, we see a similar change in the shape of peaks
-% of the position signal as when comparing the shape of the velocity
-% signal's peaks using a second and a third order filter. This means that,
-% in addition to possibly introducing extra glissades into the data, we
-% also catch slightly longer saccades of slightly lower velocity using the
-% Savitzky-Golay filter.
-% Other filtering methods might bring resolve here, such as possibly an
-% optimal (Wiener) filter, or some other more advanced and recently
-% developed methods. I am not knowledgable on this subject matter though,
-% so I'll need to find a DSP guy to resolve this with once and for all
-% (hopefully).
-% In general, we'll need to assess how problematic this really is. When it
-% comes to the filter, we need to simulate its performance (how much
-% widening, how much undershoot is there really?). Generate a function much
-% like the eye movement, but without any noise (Gaussian velocity profile,
-% very low std for sharp peak? Or more directly cut a part from some
-% eyemovement data and smooth it by hand) and see what happens to it when
-% filtering by polynomial fit... Also, remember the Luo 2005 paper on
-% properties of Savitzky-Golay differentiation filters. By the way, i
-% indeed think my logic above was correct: the zeroth order moment of the
-% first derivative of a time series should correspond/be relatable to the
-% first order moment of the time series itself. It is thus a problem that
-% the width of the velocity peak (3rd moment) is not present by a second
-% order polynomial fit.
-%
-% Also look at:
-% filtering / digital differentiation for the eye data, look at Pavel
-% Holoborodko's stuff: http://www.holoborodko.com/pavel/numerical-methods/.
-% Not published I think, but it might be worth it, and it seems possible to
-% contact the guy, though he might not respond quickly...
-% http://nl.wikipedia.org/wiki/Savitsky-Golay_filter
 
 
 % calculate component velocities and accelerations
@@ -152,7 +101,7 @@ else
     
 end
     
-% diff pupil size. always use savitzky golay as some smoothing is needed to
+% diff pupil size. always use Savitzky-Golay as some smoothing is needed to
 % use this change of pupil size in a meaningful way for blink detection
 if isfield(data,'pupil') && ~isempty(data.pupil.size)
     if ETparams.data.qNumericallyDifferentiate==1 && 0
@@ -170,76 +119,56 @@ end
 
 % compute eye velocities, and accelerations
 %--------------------------------------------------------------------------
-if ETparams.data.qPreciseCalcDeriv
-    % TODO: implement, acceleration still TODO
-    % also TODO: check signs! For Haslwanter, positive corresponds to
-    % leftward azimuth, downward elevation and clockwise torsion. As noted
-    % in prepareData and below, our axes do not form a consistent
-    % right-handed system and should thus be calculating the wrong axis
-    % here...
-    % revise our coordinate system first when needed!
-    error('TODO')
-    
-    % Now calculate eye velocity and acceleration precisely
-    % See Equation 30 in Haslwanter T (1995) Mathematics of 3-dimensional
-    % eye rotations, Vision Res 35, 1727-1739. (look up that original
-    % Goldstein reference as well).
-    % This allows us to use the analytical differentiation of the
-    % polynomial fitted to eye azimuth and elevation (so-called coordinate
-    % velocities).
-    % The eye velocity vector is commonly called omega, where the magnitude
-    % of omega is the angular velocity and its axis indicates the
-    % instantaneous axis of the eye rotation. Omega, a vector in the
-    % head-fixed reference frame, is ordered Z-X-Y where a vector the Z
-    % direction points in the primary position, an X vector points leftward
-    % and an Y vector upward. A rotation along Z is roll/torsion, along X
-    % is pitch/elevation and along Y is yaw/azimuth (see Haslwanter 1995,
-    % Eq. 1 and Figure 1). As noted in prepareData, our current head
-    % reference axes do not conform nor even form a consistently
-    % righthanded system. When interested in using this code, revise our
-    % axes!
-    
-    % see also http://www.u.arizona.edu/~pen/ame553/lessons.html, lesson 10
-    % (see also the textbook at http://www.u.arizona.edu/~pen/ame553/)
-    % -> has acceleration as a quaternion! Textbook (appendix) has
-    % equivalence formulas for velocity as quaternion and other schemes if
-    % i remember correctly.
-    % also: 
-    % http://www.rst.e-technik.tu-dortmund.de/cms/Medienpool/Downloads/Lehre/Vorlesungen/Robotics_Theory/Diff_Kinematics_4.pdf
-    
-    % formula 30 in Haslwanter is for 3D rotation, but we have no knowledge
-    % of the torsional component, so we'll have to do with that set to 0
-    if 0
-        % if we had 3D data, and tempV(:,3) is the torsional velocity:
-        data.deg.omega = [...
-            tempV(:,3).*cosd(data.deg.Azi).*cosd(data.deg.Ele) - tempV(:,2).*sind(data.deg.Azi)                     , ...
-            tempV(:,2).*cosd(data.deg.Azi)                     + tempV(:,3).*sind(data.deg.Azi).*cosd(data.deg.ele) , ...
-            tempV(:,1)                                         - tempV(:,3).*sind(data.deg.Ele)                       ...
-            ];
-    else
-        % for lack of information about torsion, we set those parts to 0
-        % this means the instantaneous axis does not take changes in
-        % torsion into account and might therefore be systematically off.
-        % NOTE, that's a crap idea. below in else block has correct way to
-        % do velocity in Fick system
-        data.deg.omega = [...
-                                                               - tempV(:,2).*sind(data.deg.Azi)                     , ...
-            tempV(:,2).*cosd(data.deg.Azi)                                                                          , ...
-            tempV(:,1)                                                                                                ...
-            ];
-    end
-    data.deg.vel    = sqrt(sum(data.deg.omega.^2,2));
-else
-    % Calculate eye velocity and acceleration straightforwardly by applying
-    % Pythagoras' theorem. This gives us no information about the
-    % instantaneous axis of the eye rotation, but eye velocity is
-    % calculated correctly. Apply scale for velocity, as a 10° azimuth
-    % rotation at 0° elevation does not cover same distance as it does at
-    % 45° elevation: sqrt(theta_dot^2*cos^2 phi + phi_dot^2)
-    % No idea what scaling to apply for acceleration, figure that out some
-    % time...
+% Calculate eye velocity and acceleration straightforwardly by applying
+% Pythagoras' theorem. This gives us no information about the
+% instantaneous axis of the eye rotation, but eye velocity is
+% calculated correctly. Apply scale for velocity, as a 10° azimuth
+% rotation at 0° elevation does not cover same distance as it does at
+% 45° elevation: sqrt(theta_dot^2*cos^2 phi + phi_dot^2)
+if 0
+    % this is as per Warren, R. (1982). Optical transformations during
+    % movement: review of the optical concomitants of egospeed. Report
+    % AFOSR-TR-82-1028, Columbus: Ohio State University, Department of
+    % Psychology, Aviation Psychology Laboratory.
     data.deg.vel    = hypot(tempV(:,1).*cosd(data.deg.Ele), tempV(:,2));
+    % No idea what scaling to apply for acceleration, figure that out
+    % some time...
     data.deg.acc    = hypot(tempA(:,1), tempA(:,2));
+else
+    % use method as per Singh, T., Perry, C., & Herter, T. (2015). A
+    % geometric method for computing ocular kinematics and classifying
+    % gaze events using monocular remote eye tracking in a robotic
+    % environment. Journal of Neuroengineering and Rehabilitation.
+    % This uses a different spherical coordinate system, but that
+    % doesn't matter as we the final outcome of angular velocity
+    % computations is the same, and they also provide a computation for
+    % acceleration!
+    % 1. prep input
+    pixPerMeter = ETparams.screen.resolution ./ ETparams.screen.size;
+    px          = data.pix.X./pixPerMeter(1);
+    py          = data.pix.Y./pixPerMeter(2);
+    pz          = repmat(ETparams.screen.viewingDist,size(px));
+    hpxpy       = hypot(px,py);
+    rho         = hypot(hpxpy,pz);
+    theta       = atan2(py,px);
+    phi         = acos(pz./rho);
+    % 2. calculate derivatives
+    pxd         = -sgFilt(px,1,ntaps) * ETparams.samplingFreq;
+    pyd         = -sgFilt(py,1,ntaps) * ETparams.samplingFreq;
+    pzd         = 0;    % derivative of constant is 0, so just hardcode that
+    % 3. calculate component velocities as per eqs. 4a and 4b in Singh
+    % et al. (2015)
+    theta_d     = (pxd.*py-px.*pyd)./(px.^2+py.^2);
+    phi_d       = pz.*(px.*pxd+py.*pyd)./(rho.^2.*sqrt(px.^2+py.^2));       % simplified from equation in paper because pzd==0
+    rho_d       = (px.*pxd+py.*pyd+pz.*pzd)./rho;
+    % 4. calculate angular velocity
+    data.deg.vel= hypot(theta_d.*sin(phi),phi_d)*180/pi;
+    % 5. calculate angular acceleration
+    theta_dd    = conv2(theta_d,[1 0 -1].','valid')/2 * ETparams.samplingFreq; theta_dd = theta_dd([1 1:end end]);
+    phi_dd      = conv2(  phi_d,[1 0 -1].','valid')/2 * ETparams.samplingFreq;   phi_dd =   phi_dd([1 1:end end]);
+    thetaTerm   = 2.*rho_d.*theta_d.*sin(phi)./rho+theta_dd.*sin(phi)-2.*theta_d.*phi_d.*cos(phi);
+    phiTerm     = 2.*rho_d.*phi_d./rho+phi_d.^2.*sin(phi).*cos(phi)+phi_dd;
+    data.deg.acc= hypot(thetaTerm,phiTerm)*180/pi;
 end
 
 if ETparams.data.qAlsoStoreComponentDerivs
